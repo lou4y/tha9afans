39f8ae21f2f23b4f1d45f4f4a170fc6ee3d722fb
JALLOULI-IBRAHIM <ibrahim.jallouli@esprit.tn>
2023-03-09 21:38:02 +0100

    test

--- src/tha9afans/flat_earthers/main/java/test/MainFx.java
+++ src/tha9afans/flat_earthers/main/java/test/MainFx.java
@@ -1,3 +1,4 @@
+
 package test;
 
 import javafx.application.Application;
@@ -10,7 +11,7 @@ public class MainFx extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 420);
+        Scene scene = new Scene(fxmlLoader.load(), 1400, 700);
         stage.setTitle("Sign in!");
         stage.setScene(scene);
         stage.show();
